b5d06589ab63021f943045a347aae39f0baaad7d
Paul Ladd <vlendle@gmail.com>
2021-03-15 17:45:05 +0000

    Added a hello page

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/howdy', (req, res) => {
+  res.send('<html><body>Howdy, <h1>PARDNER</h1></body></html>');
+});
+
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}`);
 });
