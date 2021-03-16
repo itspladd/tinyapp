35c0b9a63b93edc2c413123ae5022229c28e8d76
Paul Ladd <vlendle@gmail.com>
2021-03-16 14:47:22 +0000

    Added lowercase letters and Z to random string

--- express_server.js
+++ express_server.js
@@ -79,8 +79,8 @@ const randomString = () => {
   let str = "";
 
   while (str.length < 6) {
-    code = randInt(48, 90);
-    if (!(code > 57 && code < 65)) {
+    code = randInt(48, 122);
+    if (!(code > 57 && code < 65) && !(code > 90 && code < 97)) {
       str += String.fromCharCode(code);
     }
   }
@@ -90,5 +90,5 @@ const randomString = () => {
 
 // Generates a random integer between two values.
 const randInt = (min, max) => {
-  return Math.floor((Math.random() * (max-min))) + min;
+  return Math.floor((Math.random() * (max-min + 1))) + min;
 }
