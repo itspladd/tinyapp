80a36810afd948692feeb9bb405c921531b78225
Paul Ladd <vlendle@gmail.com>
2021-03-17 18:00:44 +0000

    Added constants with baseline templatevars for each page

---
+++ constants.js
@@ -0,0 +1,31 @@
+const PORT = 8080;
+
+const titleBase = 'BabyURL:'
+
+const TEMPLATEVARS = {
+  home: {
+    title: `${titleBase} A URL Shortener`,
+  },
+  about: {
+    title: `${titleBase} About`,
+  },
+  register: {
+    title: `${titleBase} Register`,
+  },
+  urls_index: {
+    title: `${titleBase} My URLs`,
+  },
+  urls_new: {
+    title: `${titleBase} Make a New URL`,
+  },
+  urls_show: {
+    title: `${titleBase} URL ID`,
+  },
+  bad_url: {
+    title: `${titleBase} Short URL Not Found`,
+  },
+};
+
+module.exports = {
+  TEMPLATEVARS,
+};
